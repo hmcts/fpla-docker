a1075ed3be37697ec51318f337485f93711cdddb
luigibk <41112628+luigibk@users.noreply.github.com>
2020-09-03 12:00:44 +0100

    Jenkins agents volumes compatibility (#29)

--- bin/utils/fpl-process-definition.sh
+++ bin/utils/fpl-process-definition.sh
@@ -10,13 +10,22 @@ additionalParameters=${3-}
 
 definition_input_dir=${definition_dir}
 
-if [[ ! -e ${definition_output_file} ]]; then
-   touch ${definition_output_file}
-fi
+[[ ! -d /tmp/jenkins-agent ]] && mkdir -p /tmp/jenkins-agent
+definition_tmp=$(mktemp -d /tmp/jenkins-agent/fpla.XXXXXX)
+definition_tmp_dir="$definition_tmp/ccd-definition"
+mkdir -p "$definition_tmp_dir"
+cp -a ${definition_input_dir}/* "$definition_tmp_dir"
+definition_tmp_out_dir="${definition_tmp_dir}/build/ccd-development-config"
+[[ ! -d "$definition_tmp_out_dir" ]] && mkdir -p "$definition_tmp_out_dir"
+
+definition_tmp_out_file="${definition_tmp_out_dir}/ccd-fpl-dev.xlsx"
+[[ ! -e "$definition_tmp_out_file" ]] && touch "$definition_tmp_out_file"
 
 docker run --rm --name json2xlsx \
-  -v ${definition_input_dir}:/tmp/jenkins-agent/ccd-definition \
-  -v ${definition_output_file}:/tmp/jenkins-agent/ccd-definition.xlsx \
+  -v ${definition_tmp_dir}:/tmp/ccd-definition \
+  -v ${definition_tmp_out_file}:/tmp/ccd-definition.xlsx \
   -e CCD_DEF_CASE_SERVICE_BASE_URL=${CCD_DEF_CASE_SERVICE_BASE_URL:-http://docker.for.mac.localhost:4000} \
   hmctspublic.azurecr.io/ccd/definition-processor:${definition_processor_version} \
-  json2xlsx -D /tmp/jenkins-agent/ccd-definition -o /tmp/jenkins-agent/ccd-definition.xlsx ${additionalParameters}
+  json2xlsx -D /tmp/ccd-definition -o /tmp/ccd-definition.xlsx ${additionalParameters}
+
+cp "$definition_tmp_out_file"  "$definition_output_file"
