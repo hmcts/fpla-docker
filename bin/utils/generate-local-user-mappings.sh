478d22eab6fbe6ba3ec179305d76825a47084571
Lewis Birks <lewisb@kainos.com>
2021-03-23 15:21:20 +0000

    update mappings

--- bin/utils/generate-local-user-mappings.sh
+++ bin/utils/generate-local-user-mappings.sh
@@ -6,13 +6,16 @@ dir=$(dirname ${0})
 root_dir=$(realpath ${dir}/../..)
 
 mappings_template_dir=${root_dir}/mocks/wiremock/templates
+mappings_dir=${root_dir}/mocks/wiremock/mappings
 user_template_file=${mappings_template_dir}/userTemplate.json
 mock_user_by_email_template=${mappings_template_dir}/userByEmail.json
 mock_org_by_email_template=${mappings_template_dir}/organisationByEmail.json
 mock_org_users_by_email_template=${mappings_template_dir}/organisationUsersByEmail.json
-mock_user_by_email_dir=${root_dir}/mocks/wiremock/mappings/prd/user-by-email
-mock_org_by_email_dir=${root_dir}/mocks/wiremock/mappings/prd/org-by-email
-mock_org_users_by_email_dir=${root_dir}/mocks/wiremock/mappings/prd/org-users-by-email
+mock_org_users_by_org_id_template=${mappings_template_dir}/organisationUsersByOrgId.json
+mock_user_by_email_dir=${mappings_dir}/prd/user-by-email
+mock_org_by_email_dir=${mappings_dir}/prd/org-by-email
+mock_org_users_by_email_dir=${mappings_dir}/prd/org-users-by-email
+mock_org_users_by_org_id_dir=${mappings_dir}/prd/org-by-org_id
 mock_file=${root_dir}/mocks/wiremock/__files/organisationUsers.json
 mock_file_dir=${root_dir}/mocks/wiremock/__files/prd/generated
 mock_tmp_file=${root_dir}/mocks/wiremock/__files/prd/generated/organisationUsers.tmp.json
@@ -25,7 +28,7 @@ query_db() {
   docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "$1" openidm
 }
 
-function get_users_email_id_mappings() {
+get_users_email_id_mappings() {
   query_db "SELECT json_object(array_agg(fullobject->>'mail'), array_agg(fullobject->>'_id')) FROM managedObjects WHERE fullobject->>'mail' IS NOT NULL AND fullobject->>'userName' LIKE '%$1%';"
 }
 
@@ -116,7 +119,20 @@ create_orgs_users_by_email_mapping() {
   rm $users_ids_tmp_file
 }
 
+create_orgs_by_id_mapping() {
+  rm -rf $mock_org_users_by_org_id_dir
+  mkdir -p $mock_org_users_by_org_id_dir
+
+  for org in "${orgs[@]}"
+  do
+    org_id=$(jq -r ".${org}" "$org_ids_file")
+    orgByIdMapping=$(sed -e "s|\[org]|$org|" -e "s|\[id]|$org_id|" "$mock_org_users_by_org_id_template")
+    echo "$orgByIdMapping" | jq '.' > "$mock_org_users_by_org_id_dir/${org}.json"
+  done
+}
+
 create_users_in_org
 create_user_by_email_mapping
 create_orgs_by_email_mapping
 create_orgs_users_by_email_mapping
+create_orgs_by_id_mapping
