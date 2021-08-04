6e67d740aa1e8746ada449cf7825e9de945a913f
Tomasz Powroznik <tomas.powroznik@gmail.com>
2021-08-04 10:29:10 +0100

    chore: Adopt to recent ccd changes (ccd system user)

--- bin/add-services.sh
+++ bin/add-services.sh
@@ -4,6 +4,8 @@ set -eu
 
 dir=$(dirname ${0})
 
+${dir}/utils/idam-create-service.sh "ccd_data_store_api" "ccd_data_store_api" "idam_data_store_client_secret" "http://ccd-data-store-api/oauth2redirect" "false" "profile openid roles manage-user"
+
 ${dir}/utils/idam-create-service.sh "ccd_gateway" "ccd_gateway" "ccd_gateway_secret" "http://localhost:3451/oauth2redirect"
 
 ${dir}/utils/idam-create-service.sh "fpl" "fpl" "OOOOOOOOOOOOOOOO" "https://localhost:9000/oauth2/callback"
