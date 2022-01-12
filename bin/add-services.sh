9c92f8efe3e75a41a398bdf56909638fbe2c8d57
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2022-01-12 10:36:38 +0000

    DFPL-409: Update fpla-docker with updates from ccd-docker (#77)
    * update scripts + container envs
    
    * fix container envs
    
    * enable role based access control
    
    * local-env-fixes - Add .DS_Store to .gitignore file
    
    include .DS_Store in the .gitignore file to avoid
    tracking it.
    
    * fix mismatched S2S keys for case-assignment
    
    Co-authored-by: amoriartyMOJ <aaron.moriarty@justice.gov.uk>

--- bin/add-services.sh
+++ bin/add-services.sh
@@ -6,6 +6,8 @@ dir=$(dirname ${0})
 
 ${dir}/utils/idam-create-service.sh "ccd_data_store_api" "ccd_data_store_api" "idam_data_store_client_secret" "http://ccd-data-store-api/oauth2redirect" "false" "profile openid roles manage-user"
 
+${dir}/utils/idam-create-service.sh "ccd_gateway" "ccd_gateway" "ccd_gateway_secret" "http://localhost:3451/oauth2redirect"
+
 ${dir}/utils/idam-create-service.sh "fpl" "fpl" "OOOOOOOOOOOOOOOO" "https://localhost:9000/oauth2/callback"
 
 ${dir}/utils/idam-create-service.sh "xui_webapp" "xui_webapp" "OOOOOOOOOOOOOOOO" "http://localhost:3333/oauth2/callback" "false" "profile openid roles manage-user create-user search-user"
